5dd79d1a6f3e2bc34ec75a7ca2a337bb77f2f2a0
Walkeruru <santiagoolivera.soly1@gmail.com>
2023-04-11 18:11:58 -0300

    changed class component to funcional components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react'
+import React, { useState, useRef, useEffect } from 'react';
 import './styles/App.css';
 import Informacion from './components/Informacion';
 import Educacion from './components/Educacion';
@@ -8,63 +8,44 @@ import Container  from '@mui/material/Container';
 import  Typography  from '@mui/material/Typography';
 import  Box  from '@mui/material/Box';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const printRef = useRef('');
+  const [previewBtnActive, setPreviewBtnActive] = useState(false);
+  const [printBtnActive, setPrintBtnActive] = useState(false);
 
-    this.state = {
-      previewBotonActive: false,
-      printBotonActive: false,
-    }
-    
-    this.handleClick = this.handleClick.bind(this)
-    this.handlePrint = this.handlePrint.bind(this)
-    }
+  useEffect(() => {
+    printRef.current = printBtnActive;
+  }, [printBtnActive]);
 
-  handleClick(){
-    this.setState({
-      previewBotonActive: !this.state.previewBotonActive
-    })
-  }
-  handlePrint(){
-    this.setState({
-      printBotonActive: !this.state.printBotonActive
-    })
-    setTimeout(() => {
+  const handleClick = () => {return setPreviewBtnActive(!previewBtnActive)}
+  const handlePrint = () => {
+    setPrintBtnActive(!printBtnActive)
+      setTimeout(() => {
       print();
-      this.setState({
-        printBotonActive: !this.state.printBotonActive
-      })
+      setPrintBtnActive(!printRef.current);
     }, "500");
   }
 
-  render(){
-    let buttonPrint
-    let buttonEdit
-    let logo
-    if(!this.state.printBotonActive){
-      buttonPrint = <Button onClick={this.handlePrint}>Print</Button>
-      buttonEdit = <Button onClick={this.handleClick}>{this.state.previewBotonActive ? "Editar" : "Preview CV"}</Button>
-      logo = <Typography variant='h1' sx={{my:2, textAlign:'center', fontFamily:'Nabla'}}>CV Builder</Typography>
-    }
-    return (
+  return(
+    <>
       <div className="App">
         <Box sx={{boxShadow:'0 15px 30px rgba(0,0,0,0.15)', borderRadius:'20px', padding:'25px 0', margin:'25px 0' , width:{md:'70%'}}}>
           <Container>
-            {logo}
+            {!printBtnActive ? <Typography variant='h1' sx={{my:2, textAlign:'center', fontFamily:'Nabla'}}>CV Builder</Typography> : null }
             <hr />
           </Container>
-          <Informacion preview={this.state.previewBotonActive} print={this.state.printBotonActive}></Informacion>
-          <Educacion preview={this.state.previewBotonActive} print={this.state.printBotonActive}></Educacion>
-          <Experiencia preview={this.state.previewBotonActive} print={this.state.printBotonActive}></Experiencia>
+          <Informacion preview={previewBtnActive} print={printBtnActive}></Informacion>
+          <Educacion preview={previewBtnActive} print={printBtnActive}></Educacion>
+          <Experiencia preview={previewBtnActive} print={printBtnActive}></Experiencia>
           <Container>
-            {buttonEdit}
-            {buttonPrint}
+            {!printBtnActive ?  <Button onClick={handleClick}>{previewBtnActive ? "Editar" : "Preview CV"}</Button> : null}
+            {!printBtnActive ? <Button onClick={handlePrint}>Print</Button> : null}
           </Container>
         </Box>
       </div>
-    )
-  }
+    </>
+  )
 }
+
 
 export default App
